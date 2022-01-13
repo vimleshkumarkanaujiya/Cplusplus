64f962538730c54dadf2c3c53fe30a05069be9ac
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2022-01-14 03:45:57 +0530

    A new file created to show the quadratic roots.

--- Quadratic_equation.cpp
+++ Quadratic_equation.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<cmath>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main(){
 	float a,b,c,x1,x2,discriminant,realPart,imaginaryPart;
 	cout<<"Enter the coefficients \n";
