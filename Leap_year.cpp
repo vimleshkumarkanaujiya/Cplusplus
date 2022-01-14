1f9e304e28931560acdfc80c9b0349a16bcb4f86
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2022-01-15 00:26:24 +0530

    Created a new file Leap_year.cpp

---
+++ Leap_year.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int year;
+   
+   cout<<"Enter year: ";
+   cin>>year;
+   
+   if(year%4==0){
+	 if(year%100==0){
+		   if(year%400==0)
+		   cout<<"This is a leap year";
+		   else
+		   cout<<"This is not a leap year";
+		   }else
+			   cout<<"This is not a leap year";
+	}else
+		cout<<"This is not a leap year";
+		return 0;
+}
