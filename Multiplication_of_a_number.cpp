05d88fd7cc4f61763aa153cfa7da061add0f473c
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2022-01-25 01:44:42 +0530

    Created a file Multiplication_of_a_number.cpp

---
+++ Multiplication_of_a_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,m;
+	cout<<"Enter the number: ";
+	cin>>n;
+	
+	for(int i=1;i<=10;i++){
+		m=n*i;
+		cout<<"\n"<<m<<"\n";
+	}
+	return 0;
+}
