a19284e30e021d8fddecdb5fe17f465b9ea31def
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2022-05-03 02:31:05 +0530

    Created a file Hollow_rectangle_star.cpp

---
+++ Hollow_rectangle_star.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int j=1; j<=n; j++)
+	{
+		cout<<'\n';
+	for(int i=1;i<=n; i++)
+	{
+		if(j==1 || j==n || i==1 || i==n){
+		cout<<"*";
+	    }
+	    else
+	    {
+		cout<<" ";
+	    }
+    }
+    }
+	return 0;
+}
