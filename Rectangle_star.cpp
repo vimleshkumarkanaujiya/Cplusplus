4d424a67ca44c5d1e396db299708b0b7c4d541f6
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2022-05-03 02:33:02 +0530

    Created a file Rectangle.cpp

---
+++ Rectangle_star.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int j=1; j<=n; j++)
+	{
+	cout<<'\n';
+	for(int i=1;i<=n; i++)
+	{
+	cout<<"*";	
+    }
+    }
+	return 0;
+}
