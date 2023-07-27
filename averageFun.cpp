e5e4d596706f204c965db9fc58de4bc16ea0cc0b
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-07-28 01:10:31 +0530

    Update averageFun.cpp
    Changed variable names, created pointers, and calculated the array size.

--- averageFun.cpp
+++ averageFun.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
 
-double averageCost(double *priceArray, int count){
-		double sum = 0;
-		for(int i = 0; i< count; i++){
-			sum += *(priceArray + i);
-		}
-		return sum / count;
+int averageFun(int *arr, int *n){
+	int sum = 0;
+	for(int i = 0; i < *n; ++i){
+		sum += *(arr + i);
 	}
+	
+	return sum / *n;
+}
 
 int main(){
-	double prices[5]{5.00, 4.50, 3.75, 3.10, 6.75};
-	double average = averageCost(prices, 5);
-	cout << "$" << average << endl;
+	int array[]{1, 2, 3, 4, 5};
+	int quant = sizeof(array)/sizeof(int);
+	int average = averageFun(array, &quant);
+	cout << average << endl;
+	cout << quant;
 	
 	return 0;
 }
+
+/* Output 
+3
+*/	
